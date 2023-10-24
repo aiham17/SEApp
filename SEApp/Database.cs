eaa812e72c737203c8b77ab92169dc4bda122c1c
Aiham Jag <m.aiham@yahoo.com>
2023-10-24 10:51:31 +0100

    Fixed issue in saveUserInfo method
    In the saveUserInfo method, replaced the line `addUser.Parameters.Add(SQLQuery);` with individual parameter additions using `addUser.Parameters.AddWithValue()` to correctly bind parameters for the SQL query execution. This ensures that user information is properly saved to the database.

--- SEApp/Database.cs
+++ SEApp/Database.cs
@@ -6,54 +6,65 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+using System.CodeDom.Compiler;
+using System.Collections;
 
 namespace SEApp
 {
     class Database
     {
         private static Database dbGetString;
-
         private string dbConnectstr;
 
-        // Initialises dbConnectstr to the DBConnectionString (stores it)
+        // Constructor initializes dbConnectstr to the DBConnectionString (stores it)
         private Database()
         {
             dbConnectstr = Properties.Settings.Default.DBConnectionString;
         }
 
-        // Get Access to DB (Run the method to get connection string)
+        // Get access to DB (Run the method to get connection string)
         public static Database getConnectString()
         {
             if (dbGetString == null)
             {
-                dbGetString = new Database(); 
+                dbGetString = new Database();
             }
             return dbGetString;
         }
 
 
-        // Saves UserInfo inputted in the registration form to the Database
-        // NEED TO LOOK INTO PASSING A STRUCT AS THE PARAMETER RATHER THAN INDIVDUAL VARIABLES
-        
-        public void saveUserInfo(string SQLQuery, string userName, string password, string firstName, string lastName, string email, int CompanyRole)
+        // Fixed issue in saveUserInfo method
+        // In the saveUserInfo method, replaced the line `addUser.Parameters.Add(SQLQuery);`
+        // with individual parameter additions using `addUser.Parameters.AddWithValue()` to correctly bind parameters for the SQL query execution.
+        // This ensures that user information is properly saved to the database.
+
+
+        public void saveUserInfo(string SQLQuery, string userName, string password, string salt, string firstName, string lastName, string email, int CompanyRole)
+    {
+        List<string> parameterNames = new List<string> { "@Username", "@Password", "@Salt", "@FirstName", "@LastName", "@Email", "@CompanyRole" };
+        using (SqlConnection connectDB = new SqlConnection(dbConnectstr))
         {
-            //Store the columns of the DB. Will be creating one for the variables and gonna try a for loop.
-            List<string> columns = new List<string>{"Username", "Password", "FirstName", "LastName", "Email", "CompanyRole"};
-            
-            using (SqlConnection connectDB = new SqlConnection(dbConnectstr))
-            {
                 connectDB.Open();
                 SqlCommand addUser = new SqlCommand(SQLQuery, connectDB);
 
                 addUser.CommandType = CommandType.Text;
-                addUser.Parameters.Add(SQLQuery);
+
+                // Loop through parameter names and add them to the command with their corresponding values
+                for (int i = 0; i < parameterNames.Count; i++)
+                {
+                    addUser.Parameters.AddWithValue(parameterNames[i], i == 0 ? userName : (i == 1 ? password : (i == 2 ? salt : (i == 3 ? firstName : (i == 4 ? lastName : (i == 5 ? email : CompanyRole))))));
+                }
+
                 addUser.ExecuteNonQuery();
-
             }
+            // Display a message box indicating successful registration
 
-            
-
-
+            MessageBox.Show("User registered successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+
+
+
+
     }
 }
