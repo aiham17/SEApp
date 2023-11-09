997390e3ca6ed8be5b1552b66f1fca8990f60ae2
NoteUnworthy <118827590+NoteUnworthy@users.noreply.github.com>
2023-11-09 15:27:11 +0000

    CF - made displays for 'Login & Security' and 'Personal Details' Pages

--- SEApp/Dashboard.Designer.cs
+++ SEApp/Dashboard.Designer.cs
@@ -68,9 +68,8 @@
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(-2, -2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(196, 102);
+            this.pictureBox1.Size = new System.Drawing.Size(147, 83);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -79,10 +78,9 @@
             // btnAddOrAdjustVendors
             // 
             this.btnAddOrAdjustVendors.BackColor = System.Drawing.SystemColors.Window;
-            this.btnAddOrAdjustVendors.Location = new System.Drawing.Point(-2, 214);
-            this.btnAddOrAdjustVendors.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddOrAdjustVendors.Location = new System.Drawing.Point(-2, 174);
             this.btnAddOrAdjustVendors.Name = "btnAddOrAdjustVendors";
-            this.btnAddOrAdjustVendors.Size = new System.Drawing.Size(196, 50);
+            this.btnAddOrAdjustVendors.Size = new System.Drawing.Size(147, 41);
             this.btnAddOrAdjustVendors.TabIndex = 3;
             this.btnAddOrAdjustVendors.Text = "Add or Adjust Vendors";
             this.btnAddOrAdjustVendors.UseVisualStyleBackColor = false;
@@ -92,10 +90,9 @@
             // 
             this.btnSetting.BackColor = System.Drawing.SystemColors.Window;
             this.btnSetting.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnSetting.Location = new System.Drawing.Point(-2, 400);
-            this.btnSetting.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSetting.Location = new System.Drawing.Point(-2, 325);
             this.btnSetting.Name = "btnSetting";
-            this.btnSetting.Size = new System.Drawing.Size(196, 50);
+            this.btnSetting.Size = new System.Drawing.Size(147, 41);
             this.btnSetting.TabIndex = 4;
             this.btnSetting.Text = "Settings";
             this.btnSetting.UseVisualStyleBackColor = false;
@@ -104,10 +101,9 @@
             // btnVendorsAndProducts
             // 
             this.btnVendorsAndProducts.BackColor = System.Drawing.SystemColors.Window;
-            this.btnVendorsAndProducts.Location = new System.Drawing.Point(-2, 158);
-            this.btnVendorsAndProducts.Margin = new System.Windows.Forms.Padding(4);
+            this.btnVendorsAndProducts.Location = new System.Drawing.Point(-2, 128);
             this.btnVendorsAndProducts.Name = "btnVendorsAndProducts";
-            this.btnVendorsAndProducts.Size = new System.Drawing.Size(196, 50);
+            this.btnVendorsAndProducts.Size = new System.Drawing.Size(147, 41);
             this.btnVendorsAndProducts.TabIndex = 15;
             this.btnVendorsAndProducts.Text = "Vendors and Products";
             this.btnVendorsAndProducts.UseVisualStyleBackColor = false;
@@ -116,10 +112,9 @@
             // btnDashBoard
             // 
             this.btnDashBoard.BackColor = System.Drawing.SystemColors.Window;
-            this.btnDashBoard.Location = new System.Drawing.Point(-2, 86);
-            this.btnDashBoard.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDashBoard.Location = new System.Drawing.Point(-2, 70);
             this.btnDashBoard.Name = "btnDashBoard";
-            this.btnDashBoard.Size = new System.Drawing.Size(197, 48);
+            this.btnDashBoard.Size = new System.Drawing.Size(148, 39);
             this.btnDashBoard.TabIndex = 16;
             this.btnDashBoard.Text = "DashBoard";
             this.btnDashBoard.UseVisualStyleBackColor = false;
@@ -129,10 +124,9 @@
             // 
             this.btnLogout.BackColor = System.Drawing.SystemColors.Window;
             this.btnLogout.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnLogout.Location = new System.Drawing.Point(-2, 507);
-            this.btnLogout.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLogout.Location = new System.Drawing.Point(-2, 412);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(196, 50);
+            this.btnLogout.Size = new System.Drawing.Size(147, 41);
             this.btnLogout.TabIndex = 20;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = false;
@@ -145,7 +139,8 @@
             this.chActiveVendors.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chActiveVendors.Legends.Add(legend1);
-            this.chActiveVendors.Location = new System.Drawing.Point(957, 25);
+            this.chActiveVendors.Location = new System.Drawing.Point(718, 20);
+            this.chActiveVendors.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chActiveVendors.Name = "chActiveVendors";
             this.chActiveVendors.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Pastel;
             series1.ChartArea = "ChartArea1";
@@ -153,7 +148,7 @@
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chActiveVendors.Series.Add(series1);
-            this.chActiveVendors.Size = new System.Drawing.Size(411, 352);
+            this.chActiveVendors.Size = new System.Drawing.Size(308, 286);
             this.chActiveVendors.TabIndex = 21;
             this.chActiveVendors.Text = "chart1";
             this.chActiveVendors.Click += new System.EventHandler(this.chActiveVendors_Click);
@@ -164,13 +159,14 @@
             this.chHPVendors.ChartAreas.Add(chartArea2);
             legend2.Name = "Legend1";
             this.chHPVendors.Legends.Add(legend2);
-            this.chHPVendors.Location = new System.Drawing.Point(215, 384);
+            this.chHPVendors.Location = new System.Drawing.Point(161, 312);
+            this.chHPVendors.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chHPVendors.Name = "chHPVendors";
             series2.ChartArea = "ChartArea1";
             series2.Legend = "Legend1";
             series2.Name = "Series1";
             this.chHPVendors.Series.Add(series2);
-            this.chHPVendors.Size = new System.Drawing.Size(550, 308);
+            this.chHPVendors.Size = new System.Drawing.Size(412, 250);
             this.chHPVendors.TabIndex = 22;
             this.chHPVendors.Text = "chart1";
             this.chHPVendors.Click += new System.EventHandler(this.chHPVendors_Click);
@@ -181,7 +177,8 @@
             this.chLPVendors.ChartAreas.Add(chartArea3);
             legend3.Name = "Legend1";
             this.chLPVendors.Legends.Add(legend3);
-            this.chLPVendors.Location = new System.Drawing.Point(620, 25);
+            this.chLPVendors.Location = new System.Drawing.Point(465, 20);
+            this.chLPVendors.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chLPVendors.Name = "chLPVendors";
             this.chLPVendors.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
             series3.ChartArea = "ChartArea1";
@@ -189,7 +186,7 @@
             series3.Legend = "Legend1";
             series3.Name = "Series1";
             this.chLPVendors.Series.Add(series3);
-            this.chLPVendors.Size = new System.Drawing.Size(331, 352);
+            this.chLPVendors.Size = new System.Drawing.Size(248, 286);
             this.chLPVendors.TabIndex = 23;
             this.chLPVendors.Text = "chart1";
             this.chLPVendors.Click += new System.EventHandler(this.chLPVendors_Click);
@@ -200,14 +197,15 @@
             this.chHighestProduct.ChartAreas.Add(chartArea4);
             legend4.Name = "Legend1";
             this.chHighestProduct.Legends.Add(legend4);
-            this.chHighestProduct.Location = new System.Drawing.Point(800, 392);
+            this.chHighestProduct.Location = new System.Drawing.Point(600, 318);
+            this.chHighestProduct.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chHighestProduct.Name = "chHighestProduct";
             series4.ChartArea = "ChartArea1";
             series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series4.Legend = "Legend1";
             series4.Name = "Series1";
             this.chHighestProduct.Series.Add(series4);
-            this.chHighestProduct.Size = new System.Drawing.Size(552, 300);
+            this.chHighestProduct.Size = new System.Drawing.Size(414, 244);
             this.chHighestProduct.TabIndex = 24;
             this.chHighestProduct.Text = "chart1";
             this.chHighestProduct.Click += new System.EventHandler(this.chHighestProduct_Click);
@@ -219,25 +217,26 @@
             this.chLowestProduct.ChartAreas.Add(chartArea5);
             legend5.Name = "Legend1";
             this.chLowestProduct.Legends.Add(legend5);
-            this.chLowestProduct.Location = new System.Drawing.Point(202, 25);
+            this.chLowestProduct.Location = new System.Drawing.Point(152, 20);
+            this.chLowestProduct.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.chLowestProduct.Name = "chLowestProduct";
             series5.ChartArea = "ChartArea1";
             series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
             series5.Legend = "Legend1";
             series5.Name = "Series1";
             this.chLowestProduct.Series.Add(series5);
-            this.chLowestProduct.Size = new System.Drawing.Size(412, 352);
+            this.chLowestProduct.Size = new System.Drawing.Size(309, 286);
             this.chLowestProduct.TabIndex = 25;
             this.chLowestProduct.Text = "chart1";
             this.chLowestProduct.Click += new System.EventHandler(this.chLowestProduct_Click);
             // 
             // Dashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1380, 694);
+            this.ClientSize = new System.Drawing.Size(1035, 564);
             this.Controls.Add(this.chLowestProduct);
             this.Controls.Add(this.chHighestProduct);
             this.Controls.Add(this.chLPVendors);
@@ -249,7 +248,6 @@
             this.Controls.Add(this.btnSetting);
             this.Controls.Add(this.btnAddOrAdjustVendors);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Dashboard";
             this.Text = "Dashbaord";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
---
+++ SEApp/Login&Security.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace SEApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.btnDashBoard = new System.Windows.Forms.Button();
+            this.btnVendorsAndProducts = new System.Windows.Forms.Button();
+            this.btnSetting = new System.Windows.Forms.Button();
+            this.btnAddOrAdjustVendors = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.SystemColors.Window;
+            this.btnLogout.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnLogout.Location = new System.Drawing.Point(-1, 412);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(147, 41);
+            this.btnLogout.TabIndex = 26;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // btnDashBoard
+            // 
+            this.btnDashBoard.BackColor = System.Drawing.SystemColors.Window;
+            this.btnDashBoard.Location = new System.Drawing.Point(-1, 70);
+            this.btnDashBoard.Name = "btnDashBoard";
+            this.btnDashBoard.Size = new System.Drawing.Size(148, 39);
+            this.btnDashBoard.TabIndex = 25;
+            this.btnDashBoard.Text = "DashBoard";
+            this.btnDashBoard.UseVisualStyleBackColor = false;
+            this.btnDashBoard.Click += new System.EventHandler(this.btnDashBoard_Click);
+            // 
+            // btnVendorsAndProducts
+            // 
+            this.btnVendorsAndProducts.BackColor = System.Drawing.SystemColors.Window;
+            this.btnVendorsAndProducts.Location = new System.Drawing.Point(-1, 128);
+            this.btnVendorsAndProducts.Name = "btnVendorsAndProducts";
+            this.btnVendorsAndProducts.Size = new System.Drawing.Size(147, 41);
+            this.btnVendorsAndProducts.TabIndex = 24;
+            this.btnVendorsAndProducts.Text = "Vendors and Products";
+            this.btnVendorsAndProducts.UseVisualStyleBackColor = false;
+            // 
+            // btnSetting
+            // 
+            this.btnSetting.BackColor = System.Drawing.SystemColors.Window;
+            this.btnSetting.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSetting.Location = new System.Drawing.Point(-1, 325);
+            this.btnSetting.Name = "btnSetting";
+            this.btnSetting.Size = new System.Drawing.Size(147, 41);
+            this.btnSetting.TabIndex = 23;
+            this.btnSetting.Text = "Settings";
+            this.btnSetting.UseVisualStyleBackColor = false;
+            // 
+            // btnAddOrAdjustVendors
+            // 
+            this.btnAddOrAdjustVendors.BackColor = System.Drawing.SystemColors.Window;
+            this.btnAddOrAdjustVendors.Location = new System.Drawing.Point(-1, 174);
+            this.btnAddOrAdjustVendors.Name = "btnAddOrAdjustVendors";
+            this.btnAddOrAdjustVendors.Size = new System.Drawing.Size(147, 41);
+            this.btnAddOrAdjustVendors.TabIndex = 22;
+            this.btnAddOrAdjustVendors.Text = "Add or Adjust Vendors";
+            this.btnAddOrAdjustVendors.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 83);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Edit User:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "UserName:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(231, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Confirm Password:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(252, 376);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Email:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(293, 269);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 20);
+            this.textBox1.TabIndex = 32;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(293, 295);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(134, 20);
+            this.textBox2.TabIndex = 33;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(293, 321);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(134, 20);
+            this.textBox3.TabIndex = 34;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(293, 347);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(134, 20);
+            this.textBox4.TabIndex = 35;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(293, 373);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(134, 20);
+            this.textBox5.TabIndex = 36;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(441, 13);
+            this.label6.TabIndex = 37;
+            this.label6.Text = "ADD A DATASET HERE, SHOWS USERNAME EMAIL AND PASSWORD IN ASTERISKS";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(352, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 38;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 26);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 39;
+            this.label7.Text = "User Details;";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(385, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 40;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(181, 249);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(267, 189);
+            this.panel1.TabIndex = 41;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Location = new System.Drawing.Point(181, 70);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(473, 164);
+            this.panel2.TabIndex = 42;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnDashBoard);
+            this.Controls.Add(this.btnVendorsAndProducts);
+            this.Controls.Add(this.btnSetting);
+            this.Controls.Add(this.btnAddOrAdjustVendors);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnLogout;
+        private Button btnDashBoard;
+        private Button btnVendorsAndProducts;
+        private Button btnSetting;
+        private Button btnAddOrAdjustVendors;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label6;
+        private Button button1;
+        private Label label7;
+        private Button button2;
+        private Panel panel1;
+        private Panel panel2;
+    }
+}
