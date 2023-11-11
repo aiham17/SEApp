98666a614e677856e4a3c4b4be711370a827ef90
Adam Dare <71335091+AdamPDare@users.noreply.github.com>
2023-11-11 11:51:24 +0000

    Added Data Grid View and adjusted names of textboxes etc to Login & Security Page

--- SEApp/Login&Security.Designer.cs
+++ SEApp/Login&Security.Designer.cs
@@ -40,28 +40,27 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label7 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.tbUserEdit = new System.Windows.Forms.TextBox();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.tbConfirmPassword = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.dgvUserData = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserData)).BeginInit();
             this.SuspendLayout();
             // 
             // btnLogout
             // 
             this.btnLogout.BackColor = System.Drawing.SystemColors.Window;
             this.btnLogout.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnLogout.Location = new System.Drawing.Point(-1, 412);
+            this.btnLogout.Location = new System.Drawing.Point(-2, 634);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(147, 41);
+            this.btnLogout.Size = new System.Drawing.Size(220, 63);
             this.btnLogout.TabIndex = 26;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = false;
@@ -69,9 +68,10 @@
             // btnDashBoard
             // 
             this.btnDashBoard.BackColor = System.Drawing.SystemColors.Window;
-            this.btnDashBoard.Location = new System.Drawing.Point(-1, 70);
+            this.btnDashBoard.Location = new System.Drawing.Point(-2, 108);
+            this.btnDashBoard.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnDashBoard.Name = "btnDashBoard";
-            this.btnDashBoard.Size = new System.Drawing.Size(148, 39);
+            this.btnDashBoard.Size = new System.Drawing.Size(222, 60);
             this.btnDashBoard.TabIndex = 25;
             this.btnDashBoard.Text = "DashBoard";
             this.btnDashBoard.UseVisualStyleBackColor = false;
@@ -80,9 +80,10 @@
             // btnVendorsAndProducts
             // 
             this.btnVendorsAndProducts.BackColor = System.Drawing.SystemColors.Window;
-            this.btnVendorsAndProducts.Location = new System.Drawing.Point(-1, 128);
+            this.btnVendorsAndProducts.Location = new System.Drawing.Point(-2, 197);
+            this.btnVendorsAndProducts.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnVendorsAndProducts.Name = "btnVendorsAndProducts";
-            this.btnVendorsAndProducts.Size = new System.Drawing.Size(147, 41);
+            this.btnVendorsAndProducts.Size = new System.Drawing.Size(220, 63);
             this.btnVendorsAndProducts.TabIndex = 24;
             this.btnVendorsAndProducts.Text = "Vendors and Products";
             this.btnVendorsAndProducts.UseVisualStyleBackColor = false;
@@ -91,9 +92,10 @@
             // 
             this.btnSetting.BackColor = System.Drawing.SystemColors.Window;
             this.btnSetting.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnSetting.Location = new System.Drawing.Point(-1, 325);
+            this.btnSetting.Location = new System.Drawing.Point(-2, 500);
+            this.btnSetting.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSetting.Name = "btnSetting";
-            this.btnSetting.Size = new System.Drawing.Size(147, 41);
+            this.btnSetting.Size = new System.Drawing.Size(220, 63);
             this.btnSetting.TabIndex = 23;
             this.btnSetting.Text = "Settings";
             this.btnSetting.UseVisualStyleBackColor = false;
@@ -101,9 +103,10 @@
             // btnAddOrAdjustVendors
             // 
             this.btnAddOrAdjustVendors.BackColor = System.Drawing.SystemColors.Window;
-            this.btnAddOrAdjustVendors.Location = new System.Drawing.Point(-1, 174);
+            this.btnAddOrAdjustVendors.Location = new System.Drawing.Point(-2, 268);
+            this.btnAddOrAdjustVendors.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnAddOrAdjustVendors.Name = "btnAddOrAdjustVendors";
-            this.btnAddOrAdjustVendors.Size = new System.Drawing.Size(147, 41);
+            this.btnAddOrAdjustVendors.Size = new System.Drawing.Size(220, 63);
             this.btnAddOrAdjustVendors.TabIndex = 22;
             this.btnAddOrAdjustVendors.Text = "Add or Adjust Vendors";
             this.btnAddOrAdjustVendors.UseVisualStyleBackColor = false;
@@ -112,9 +115,10 @@
             // 
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(147, 83);
+            this.pictureBox1.Size = new System.Drawing.Size(220, 128);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 21;
             this.pictureBox1.TabStop = false;
@@ -122,36 +126,40 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(234, 272);
+            this.label1.Location = new System.Drawing.Point(351, 418);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.Size = new System.Drawing.Size(79, 20);
             this.label1.TabIndex = 27;
             this.label1.Text = "Edit User:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(227, 298);
+            this.label2.Location = new System.Drawing.Point(340, 458);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.Size = new System.Drawing.Size(89, 20);
             this.label2.TabIndex = 28;
             this.label2.Text = "UserName:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(231, 324);
+            this.label3.Location = new System.Drawing.Point(346, 498);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.Size = new System.Drawing.Size(82, 20);
             this.label3.TabIndex = 29;
             this.label3.Text = "Password:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(193, 350);
+            this.label4.Location = new System.Drawing.Point(290, 538);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.Size = new System.Drawing.Size(141, 20);
             this.label4.TabIndex = 30;
             this.label4.Text = "Confirm Password:";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -159,114 +167,106 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(252, 376);
+            this.label5.Location = new System.Drawing.Point(378, 578);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(52, 20);
             this.label5.TabIndex = 31;
             this.label5.Text = "Email:";
             // 
-            // textBox1
+            // tbUserEdit
             // 
-            this.textBox1.Location = new System.Drawing.Point(293, 269);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(134, 20);
-            this.textBox1.TabIndex = 32;
+            this.tbUserEdit.Location = new System.Drawing.Point(440, 414);
+            this.tbUserEdit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbUserEdit.Name = "tbUserEdit";
+            this.tbUserEdit.Size = new System.Drawing.Size(199, 26);
+            this.tbUserEdit.TabIndex = 32;
             // 
-            // textBox2
+            // tbUsername
             // 
-            this.textBox2.Location = new System.Drawing.Point(293, 295);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(134, 20);
-            this.textBox2.TabIndex = 33;
+            this.tbUsername.Location = new System.Drawing.Point(440, 454);
+            this.tbUsername.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(199, 26);
+            this.tbUsername.TabIndex = 33;
             // 
-            // textBox3
+            // tbPassword
             // 
-            this.textBox3.Location = new System.Drawing.Point(293, 321);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(134, 20);
-            this.textBox3.TabIndex = 34;
+            this.tbPassword.Location = new System.Drawing.Point(440, 494);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(199, 26);
+            this.tbPassword.TabIndex = 34;
             // 
-            // textBox4
+            // tbConfirmPassword
             // 
-            this.textBox4.Location = new System.Drawing.Point(293, 347);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(134, 20);
-            this.textBox4.TabIndex = 35;
+            this.tbConfirmPassword.Location = new System.Drawing.Point(440, 534);
+            this.tbConfirmPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbConfirmPassword.Name = "tbConfirmPassword";
+            this.tbConfirmPassword.Size = new System.Drawing.Size(199, 26);
+            this.tbConfirmPassword.TabIndex = 35;
             // 
-            // textBox5
+            // tbEmail
             // 
-            this.textBox5.Location = new System.Drawing.Point(293, 373);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(134, 20);
-            this.textBox5.TabIndex = 36;
+            this.tbEmail.Location = new System.Drawing.Point(440, 574);
+            this.tbEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(199, 26);
+            this.tbEmail.TabIndex = 36;
             // 
-            // label6
+            // btnSave
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(19, 47);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(441, 13);
-            this.label6.TabIndex = 37;
-            this.label6.Text = "ADD A DATASET HERE, SHOWS USERNAME EMAIL AND PASSWORD IN ASTERISKS";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(352, 399);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 38;
-            this.button1.Text = "Save";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(19, 26);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(67, 13);
-            this.label7.TabIndex = 39;
-            this.label7.Text = "User Details;";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(385, 21);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 40;
-            this.button2.Text = "Edit";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnSave.Location = new System.Drawing.Point(528, 614);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 35);
+            this.btnSave.TabIndex = 38;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(181, 249);
+            this.panel1.Location = new System.Drawing.Point(294, 405);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(267, 189);
+            this.panel1.Size = new System.Drawing.Size(400, 273);
             this.panel1.TabIndex = 41;
             // 
-            // panel2
+            // btnEdit
             // 
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.label6);
-            this.panel2.Controls.Add(this.label7);
-            this.panel2.Location = new System.Drawing.Point(181, 70);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(473, 164);
-            this.panel2.TabIndex = 42;
+            this.btnEdit.Location = new System.Drawing.Point(865, 108);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(112, 35);
+            this.btnEdit.TabIndex = 40;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // dgvUserData
+            // 
+            this.dgvUserData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUserData.Location = new System.Drawing.Point(294, 108);
+            this.dgvUserData.Name = "dgvUserData";
+            this.dgvUserData.RowHeadersWidth = 62;
+            this.dgvUserData.RowTemplate.Height = 28;
+            this.dgvUserData.Size = new System.Drawing.Size(564, 267);
+            this.dgvUserData.TabIndex = 42;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.dgvUserData);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbConfirmPassword);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.tbUserEdit);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -279,12 +279,11 @@
             this.Controls.Add(this.btnAddOrAdjustVendors);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.panel2);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUserData)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -303,16 +302,14 @@
         private Label label3;
         private Label label4;
         private Label label5;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private TextBox textBox5;
-        private Label label6;
-        private Button button1;
-        private Label label7;
-        private Button button2;
+        private TextBox tbUserEdit;
+        private TextBox tbUsername;
+        private TextBox tbPassword;
+        private TextBox tbConfirmPassword;
+        private TextBox tbEmail;
+        private Button btnSave;
         private Panel panel1;
-        private Panel panel2;
+        private Button btnEdit;
+        private DataGridView dgvUserData;
     }
 }
