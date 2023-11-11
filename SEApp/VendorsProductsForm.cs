538166a751191c17007aa4ecffc3b0cf4338115d
Adam Dare <71335091+AdamPDare@users.noreply.github.com>
2023-11-11 13:23:19 +0000

    Export to PDF Working. Need to move to backend and fix pdf formatting

--- SEApp/VendorsProductsForm.cs
+++ SEApp/VendorsProductsForm.cs
@@ -1,5 +1,17 @@
-﻿using System.Data;
-using System.Data.SqlClient;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
 namespace SEApp
 {
     public partial class VendorsProductsForm : Form
@@ -204,12 +216,72 @@ namespace SEApp
             }
         }
 
+
+        // Exports the data in the data grid into a pdf document
+        // https://www.c-sharpcorner.com/blogs/export-datagridview-data-to-pdf-in-c-sharp
+        // Thats the source I used to find and adapt slightly the code
+        // This will need to be moved to the backend. Will do this once Ive got the formatting of the pdf correct
         private void btnExportPDF_Click(object sender, EventArgs e)
         {
             if(dgvVendorProduct.Rows.Count > 0)
             {
                 SaveFileDialog saveDT = new SaveFileDialog();
-
+                saveDT.Filter = "PDF (*.pdf)|*.pdf";
+                saveDT.FileName = "VendorProductData.pdf";
+                bool fileError = false;
+                if(saveDT.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        File.Delete(saveDT.FileName);
+                    }
+                    catch (IOException)
+                    {
+                        fileError = true;
+                        MessageBox.Show("The is a problem with the export", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                if(!fileError)
+                {
+                    try
+                    {
+                        PdfPTable pdfTable = new PdfPTable(dgvVendorProduct.Columns.Count);
+                        pdfTable.DefaultCell.Padding = 1;
+                        pdfTable.WidthPercentage = 100;
+                        pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+                        foreach (DataGridViewColumn column in dgvVendorProduct.Columns)
+                        {
+                            PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                            pdfTable.AddCell(cell);
+                        }
+                        foreach (DataGridViewRow row in dgvVendorProduct.Rows)
+                        {
+                            foreach (DataGridViewCell cell in row.Cells)
+                            {
+                                pdfTable.AddCell(cell.Value?.ToString());
+                            }
+                        }
+                        using (FileStream fstream = new FileStream(saveDT.FileName, FileMode.Create))
+                        {
+                            Document pdfDoc = new Document(PageSize.A4_LANDSCAPE, 20f, 40f, 60f, 80f);
+                            pdfDoc.SetPageSize(iTextSharp.text.PageSize.A4.Rotate());
+                            PdfWriter.GetInstance(pdfDoc, fstream);
+                            pdfDoc.Open();
+                            pdfDoc.Add(pdfTable);
+                            pdfDoc.Close();
+                            fstream.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error :" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                   
+                }
+                else
+                {
+                    MessageBox.Show("No records to export", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
         }
     }
